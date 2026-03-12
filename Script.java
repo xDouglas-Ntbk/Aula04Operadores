773433bb7322c55ff87aef81b2d8e7d8eda3afac
xDouglas-Ntbk <celularcontaminha49@gmail.com>
2026-03-12 16:20:45 -0300

    Create Script.java

---
+++ Script.java
@@ -0,0 +1,101 @@
+package exercico.devdojo.maratonajava.introducao;
+
+/*
+public class Aula04Operadores {
+    public static void main(String[] args){
+        // + - / *
+        double numero01 = 10;
+        double numero02 = 20;
+        double resultado = numero01 /(double) numero02;
+        System.out.println(resultado);
+
+        // %
+        int resto = 21 % 7;
+        System.out.println(resto);
+
+        // < > < = > = == !=     Operados de comparaçao
+
+        boolean isDezMaiorQueVinte = 10 > 20 ;
+        boolean isDezMenorQueVinte = 10 < 20 ;
+        boolean isDezIgualVinte = 10 == 20 ;
+        boolean isDezIgualDez= 10 == 10 ;
+        boolean isDezDiferente= 10 != 10 ;
+        System.out.println("isDezMaiorQueVinte "+isDezMaiorQueVinte);
+        System.out.println("isDezMenorQueVinte "+isDezMenorQueVinte);
+        System.out.println("isDezIgualVinte " +isDezIgualVinte);
+        System.out.println("isDezIgualDez " +isDezIgualDez);
+        System.out.println("ISDezDiferente "+isDezDiferente);
+    }
+}
+
+*/
+
+/*
+Crie duas variáveis int:
+
+numero1 = 20
+
+numero2 = 5
+
+Imprima:
+
+soma
+
+subtração
+
+multiplicação
+
+divisão
+ */
+
+/*
+
+// Exercicio 1 basico pedindo impressao dos calculos com operaçoes aritimeticas Nivel Fácil
+
+public class Aula04Operadores{
+    public static void main(String[] args ){
+        int numero = 20;
+        int numero2 = 5;
+        int soma =numero+numero2;
+        int subtracao = numero-numero2;
+        int multiplicacao = numero*numero2;
+        int divisao = numero/numero2;
+        System.out.println("A soma dos numero é " +soma);
+        System.out.println("A subtraçao dos numero é " +subtracao);
+        System.out.println("A multiplicaçao dos numero é " +multiplicacao);
+        System.out.println("A divisao dos numeros é " +divisao);
+    }
+}
+
+ */
+
+/*
+   // Exercicio 2 - achei facil ainda apenas com um pouco mais de variavel
+
+public class Aula04Operadores{
+    public static void main(String[] args){
+        int num1 = 10;
+        int num2 = 3;
+        int num3 = 2;
+        int somaNumeros = num1+num2+num3;
+        int multNumeros = num1+num2+num3;
+        int resultadoMult = (num1 + num2) * num3;
+        System.out.println("A soma dos três números é :"+somaNumeros);
+        System.out.println("A multiplicação dos três números é :"+multNumeros);
+        System.out.println("O resultado final é de : "+resultadoMult);
+    }
+}
+ */
+
+/*
+public class Aula04Operadores {
+    public static void main(String[] args) {
+        int precoProduto = 50;
+        int quantidade = 3;
+        int totalProduto = (precoProduto * quantidade);
+
+        System.out.println("O valor total do seu produto é : "+totalProduto);
+    }
+}
+ */
+
