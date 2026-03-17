eb640d27621387928e6c72027073e7f4741b0d2f
xDouglas-Ntbk <celularcontaminha49@gmail.com>
2026-03-17 14:23:03 -0300

    Update Script.java

--- Script.java
+++ Script.java
@@ -98,3 +98,120 @@ public class Aula04Operadores {
 }
  */
 
+
+//Essa parte aqui é sobre Operadores de Atribuiçao !!!
+/*
+public class Aula04Operadores {
+    public static void main(String[] args) {
+        // & (AND) || (or)
+        int idade = 29;
+        float salario = 3500f;
+        boolean isDentroDaLeiMaiorQueTrinta = idade >= 30 && salario >= 4612;
+        boolean isDentroDaLeiMenorqueTrinta = idade >=30 && salario >= 3381;
+
+        System.out.println("isDentroDaLeiMaiorQueTrinta "+isDentroDaLeiMaiorQueTrinta);
+        System.out.println("isDentroDaLeiMenorqueTrinta "+isDentroDaLeiMenorqueTrinta);
+
+        double valorTotalContaCorrente = 200;
+        double valorTotalContaPoupanca = 10000;
+        float valorPlaystation = 5000F;
+        boolean isPlaystationCincoCompravel  = valorTotalContaCorrente > valorPlaystation || valorTotalContaPoupanca >valorPlaystation;
+        System.out.println("isPlaystationCincoCompravel "+isPlaystationCincoCompravel);
+
+        //Valores de atribuiçao
+
+        // = += -= *= /= %=     obejetivo economizar um pouco de codigo
+
+        double bonus = 1800; // 1800
+        bonus += 1000;  // 2800
+        bonus -= 1000;  //1800
+        bonus *= 2;
+        bonus /= 2;
+        bonus %=2;
+        System.out.println(bonus);
+
+        //
+        int contador =0;
+        contador += 1;
+        System.out.println(contador);
+    }
+}
+
+ */
+
+/*
+public class Aula04Operadores {
+    public static void main(String[] args) {
+        double salario =2000;
+
+        salario += 500;
+        salario -= 300;
+        salario *= 2;
+        salario /= 2;
+
+        System.out.println(salario);
+    }
+}
+
+ */
+
+/*
+public class Aula04Operadores{
+    public static void main(String[] args) {
+
+        int pontos = 10;
+        pontos += 5;
+        pontos -= 3;
+        pontos *= 4;
+        System.out.println(pontos);
+    }
+}
+ */
+
+/*
+public class Aula04Operadores {
+    public static void main(String[] args) {
+
+        double numero = 15;
+
+        numero += 5;
+        numero *= 3;
+        numero -= 10;
+        numero /= 2;
+        numero %= 4;
+        System.out.println(numero);
+        System.out.println("O numero final é de : " +numero);
+    }
+}
+
+
+ */
+
+/*
+public class Aula04Operadores {
+    public static void main(String[] args) {
+        int contador = 0;
+        contador += 1;
+        contador += 1;
+        contador += 1;
+        contador += 1;
+        contador += 1;
+        contador -= 2;
+        System.out.println(contador);
+    }
+}
+
+ */
+
+/*
+public class Aula04Operadores {
+    public static void main(String[] args) {
+
+        double bonus = 1000;
+        bonus += 50;
+        bonus -= 25;
+        bonus %= 3;
+        System.out.println("O bonus final é no valor de : " +bonus+ "%");
+    }
+}
+*/
